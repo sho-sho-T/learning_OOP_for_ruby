5e9de7d55248fcc343c18f077298fe402412f25b
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-07-03 09:24:40 +0900

    第二章まとめ

---
+++ 02/obscuring_references.rb
@@ -0,0 +1,19 @@
+class ObscuringReferences
+    attr_reader :wheels
+    def initialize(data)
+        @wheels = wheelify(data)
+    end
+
+    def diameters
+        wheels.collect{|wheel|
+            wheel.rim + (wheel.tire * 2)}
+    end
+    # これで誰でもwheelにrim/tireを送れる
+
+    Wheel = Struct.new(:rim, :tire)
+
+    def wheelify(data)
+        data.collect {|cell|
+            Wheel.new(cell[0], cell[1])}
+    end
+end
