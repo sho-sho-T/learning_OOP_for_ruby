2d312c9fbfc1fe3edde6a5ef1473b1fb80f5b580
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-07-17 22:59:15 +0900

    第6章例１のコードを追加

---
+++ 06/6-1_1.rb
@@ -0,0 +1,23 @@
+
+class Bisycle
+  attr_reader :size, :tape_color
+
+  def initialize(args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  # 全ての自転車は、デフォルト値として
+  # 同じタイヤサイズとチェーンサイズを持つ
+
+  def spares
+    {chain: '10-speed', tire_size: '23', tape_color: tape_color}
+  end
+
+  # 他にもメソッドがたくさん
+end
+
+bike = Bicycle.new(size: 'M', tape_color: 'red')
+
+bike.size  # 'M'
+bike.spares #  {chain: '10-speed',tire_size: '23',tape_color: tape_color}
