8ebcbbcf820019e02b91c9271dc991e26c8a86a4
ShotoMatsubara <shoto0628matsubara@gmail.com>
2024-07-18 09:36:03 +0900

    コード書き途中だけどコミットしとく

---
+++ 06/6-1_2.rb
@@ -0,0 +1,22 @@
+class Bicycle
+  attr_reader :style, :size, :tape_color :front_shock, :rear_shock
+
+  def initialize(args)
+    @style = args[:style]
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+  end
+
+  # "style"の確認は、危険な道へ進む一歩
+  def spares
+    if style == :road
+      {chain: '10-speed', tire_size: '23', tape_color: tape_color}
+    else
+      {chain: '10-speed', tire_size: '2.1', rear_shock: rear_shock}
+    end
+  end
+end
+
+bike = Bicycle.new(style: :mountain, size: 'S', front_shock: 'Manitou', rear_shock: 'Fox')
